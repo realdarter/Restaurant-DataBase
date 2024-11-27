26572e3574f834e65a806c3fb9d6e0782ae4ad62
realdarter <realdarter1234@gmail.com>
2024-11-26 21:20:42 -0800

    Update MenuItem.java

--- JDBC/MenuItem.java
+++ JDBC/MenuItem.java
@@ -7,48 +7,47 @@ public class MenuItem {
     private String Category;
     
     public MenuItem(int ItemID, String Name, float Price, String Category) {
-    	this.ItemID = ItemID;
-    	this.Name = Name;
-    	this.Price = Price;
-    	this.Category = Category;
+        this.ItemID = ItemID;
+        this.Name = Name;
+        this.Price = Price;
+        this.Category = Category;
     }
     
-    //setters and getters
+    // Setters and getters
     public int getItemID() {
-    	return ItemID;
+        return ItemID;
     }
     
     public void setItemID(int ItemID) {
-    	this.ItemID = ItemID;
+        this.ItemID = ItemID;
     }
     
     public String getName() {
-    	return Name;
+        return Name;
     }
     
-    public void setItemID(String Name) {
-    	this.Name = Name;
+    public void setName(String Name) {  // Fixed the method name here
+        this.Name = Name;
     }
     
     public float getPrice() {
-    	return Price;
+        return Price;
     }
     
     public void setPrice(float Price) {
-    	this.Price = Price;
+        this.Price = Price;
     }
     
     public String getCategory() {
-    	return Category;
+        return Category;
     }
     
     public void setCategory(String Category) {
-    	this.Category = Category;
+        this.Category = Category;
     }
     
     @Override
     public String toString() {
         return "MenuItem [ItemID=" + ItemID + ", Name=" + Name + ", Price=" + Price + ", Category=" + Category + "]";
     }
-
 }
